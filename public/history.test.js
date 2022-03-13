f4fa33dbe2e9b5d0088471b0ca3501c7794731d6
Hong An Tran <68089098+tranhongan1080@users.noreply.github.com>
2022-03-13 15:24:44 -0500

    Delete history.test.js

--- public/history.test.js
+++
@@ -1,6 +0,0 @@
-const {gallonCal} = require('./history')
-
-test('tests if username and password inputed in the gui are sent to the server', async () => {
-    //expect(gallonCal()).toEqual(3);
- })
- 
